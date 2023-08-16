e3dfaeed19fed3dd8b3130d9fde94b0ee183d052
rukkyme <communicatejoy@yhaoo.com>
2023-08-16 23:49:04 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,17 +1,39 @@
-import uuid
-from typing import Union
+#!/usr/bin/env python3
+""" using Redis db """
+import redis
+from uuid import uuid4
+from typing import Union, Callable, Optional
+
+
+UnionOfTypes = Union[str, bytes, int, float]
+
 
 class Cache:
+    """creating  Class and instance"""
+
     def __init__(self):
+        """ Instance of the Redis db """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: UnionOfTypes) -> str:
+        """
+        Method takes a data argument and returns string
+        """
+        self._key = str(uuid4())
+        self._redis.set(self._key, data)
+        return self._key
 
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> UnionOfTypes:
+        """Retrieves data stored in redis using a key"""
+        value = self._redis.get(key)
+        return fn(value) if fn else value
 
-cache = Cache()
-key = cache.store("Hello, world!")
-print(f"Stored data with key: {key}")
+    def get_str(self, value: str) -> str:
+        """returns a string """
+        return self.get(self._key, str)
+
+    def get_int(self, value: str) -> int:
+        """ gets an int """
+        return self.get(self._key, int)
